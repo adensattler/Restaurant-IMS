fae1812d4a21c51b5152ef388aae5510c710c537
Aden Sattler <adensattler@gmail.com>
2024-07-24 17:20:24 -0600

    added none to empty for table vals

--- app/base/__init__.py
+++ app/base/__init__.py
@@ -3,4 +3,10 @@ from flask import Blueprint
 
 base = Blueprint('base', __name__)
 
+def none_to_empty(value):
+    return '' if value == None else value
+
+# Register the custom filter with the blueprint
+base.add_app_template_filter(none_to_empty)
+
 from . import routes
