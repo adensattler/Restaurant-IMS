a71bc0fcca06079733ed9de9ba0b366050214468
Aden Sattler <adensattler@gmail.com>
2024-07-27 17:53:36 -0600

    dt formatting (pretty)

--- app/base/__init__.py
+++ app/base/__init__.py
@@ -6,8 +6,9 @@ base = Blueprint('base', __name__)
 def none_to_empty(value):
     return '' if value == None else value
 
-def none_to_empty_price(value):
-    return f"${value}/ea" if value is not None else ''
+def none_to_empty_price(value, unit):
+
+    return f"${value}/{unit}" if value is not None else ''
 
 # Register the custom filter with the blueprint
 base.add_app_template_filter(none_to_empty)
--- app/static/js/datatable.js
+++ app/static/js/datatable.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
             },
             // Hide some columns by default
             {
-                targets: [1, 3, 5, 6, 8, 11],
+                targets: [1, 3, 5, 6, 8, 10],
                 visible: false
             }
         ],
