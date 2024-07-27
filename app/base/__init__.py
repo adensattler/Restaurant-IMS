efc63b15792ed6841686304825a458b00aa85e73
Aden Sattler <adensattler@gmail.com>
2024-07-27 17:37:31 -0600

    price formatting in dt

--- app/base/__init__.py
+++ app/base/__init__.py
@@ -6,7 +6,11 @@ base = Blueprint('base', __name__)
 def none_to_empty(value):
     return '' if value == None else value
 
+def none_to_empty_price(value):
+    return f"${value}/ea" if value is not None else ''
+
 # Register the custom filter with the blueprint
 base.add_app_template_filter(none_to_empty)
+base.add_app_template_filter(none_to_empty_price)
 
 from . import routes
