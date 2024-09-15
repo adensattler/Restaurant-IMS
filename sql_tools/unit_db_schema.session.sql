9b5228e1ca1a1b6558548bf03389e88c485ff293
Aden Sattler <adensattler@gmail.com>
2024-09-15 16:24:24 -0600

    implemented separate units table

--- sql_tools/unit_db_schema.session.sql
+++ sql_tools/unit_db_schema.session.sql
@@ -2,6 +2,15 @@
 -- DATABASE CREATION BLOCK(S)
 -- --------------------------------------------------------------------
 -- @block
+CREATE TABLE Units (
+    unit_id INT NOT NULL AUTO_INCREMENT,
+    unit_name VARCHAR(100) NOT NULL,
+    unit_abbreviation VARCHAR(10) NOT NULL,
+    precision_value INT DEFAULT 2,                 -- Precision for calculations
+    PRIMARY KEY (unit_id)
+);
+
+-- @block
 CREATE TABLE Locations (
     location_id INT NOT NULL AUTO_INCREMENT,
     location_name VARCHAR(255) NOT NULL,
@@ -19,18 +28,6 @@ CREATE TABLE Categories (
     PRIMARY KEY (category_id)
 );
 
--- @block
-INSERT INTO Units (unit_name, unit_abbreviation, conversion_to_pounds, precision)
-VALUES
-('Pounds', 'lbs', 1, 0),                    -- 1 pound equals 1 pound
-('Ounces', 'oz', 0.0625, 2),                -- 1 ounce equals 0.0625 pounds
-('Kilograms', 'kg', 2.20462, 2),            -- 1 kilogram equals 2.20462 pounds
-('Grams', 'g', 0.00220462, 3),              -- 1 gram equals 0.00220462 pounds
-('Cups', 'cups', 0.52159, 3),               -- 1 cup of water (as an example) equals 0.52159 pounds
-('Liters', 'L', 2.20462, 3),                -- 1 liter of water equals 2.20462 pounds
-('Cans', 'cans', NULL, 0),                  -- Non-convertible custom unit (NULL for conversion)
-('Packages', 'pkgs', NULL, 0);              -- Non-convertible custom unit
-
 
 -- @block
 CREATE TABLE InventoryItems (
@@ -40,13 +37,14 @@ CREATE TABLE InventoryItems (
     description TEXT,
     GTIN VARCHAR(50),
     SKU VARCHAR(255),
-    unit VARCHAR(50),
+    unit_id INT,
     weight DECIMAL(10, 2),
     price DECIMAL(10, 2),
     stock DECIMAL(10, 2),
     low_stock_level DECIMAL(10, 2),
     PRIMARY KEY (inventory_item_id),
-    FOREIGN KEY (location_id) REFERENCES Locations(location_id)
+    FOREIGN KEY (location_id) REFERENCES Locations(location_id),
+    FOREIGN KEY (unit_id) REFERENCES Units(unit_id)
 );
 
 -- @block
@@ -68,7 +66,6 @@ CREATE TABLE MenuItemComponents (
     menu_item_id INT,
     inventory_item_id INT,
     quantity_required DECIMAL(10, 2),
-    unit VARCHAR(50),
     PRIMARY KEY (component_id),
     FOREIGN KEY (menu_item_id) REFERENCES MenuItems(menu_item_id),
     FOREIGN KEY (inventory_item_id) REFERENCES InventoryItems(inventory_item_id)
@@ -78,6 +75,23 @@ CREATE TABLE MenuItemComponents (
 
 -- DATABASE INITIALIZATION/POPULATION BLOCK(S)
 -- --------------------------------------------------------------------
+-- @block
+-- Populate Units table with common units and their conversions
+INSERT INTO Units (unit_name, unit_abbreviation, precision_value)
+VALUES
+-- Common Units
+('Pound', 'lb', 2),
+('Ounce', 'oz', 2),
+('Kilogram', 'kg', 3),
+('Gram', 'g', 3),
+('Cup', 'cup', 3),
+('Liters', 'L', 3),
+('Milliliter', 'L', 2),
+('Gallon', 'gal', 2),
+
+-- Custom Units
+('Per item', 'ea', 0);
+
 -- @block
 INSERT INTO Locations (location_name, address, city, state, zipcode)
 VALUES
@@ -91,51 +105,50 @@ INSERT INTO Categories (name) VALUES
 
 -- @block
 -- Populate InventoryItems table (sample ingredients)
-INSERT INTO InventoryItems (location_id, name, description, unit, price, stock, low_stock_level)
+INSERT INTO InventoryItems (location_id, name, description, unit_id, price, stock, low_stock_level)
 VALUES
 -- Great Granny's Green Beans Ingredients
-(1, 'Canned Green Beans', 'Standard canned green beans', 'cans', 0.99, 50, 5),
-(1, 'Bullion', 'Chicken bullion', 'cups', 0.10, 30, 5),
-(1, 'Potatoes', 'Fresh potatoes', 'pieces', 0.50, 40, 10),
-(1, 'Bacon', 'Bacon strips', 'strips', 0.75, 100, 10),
+(1, 'Canned Green Beans', 'Standard canned green beans', 9, 0.99, 50, 5), -- 'Per item'
+(1, 'Bullion', 'Chicken bullion', 5, 0.10, 30, 5), -- 'Cup'
+(1, 'Potatoes', 'Fresh potatoes', 9, 0.50, 40, 10), -- 'Per item'
+(1, 'Bacon', 'Bacon strips', 9, 0.75, 100, 10), -- 'Per item'
 
 -- Sweet Baby Baked Beans Ingredients
-(1, 'Baked Beans', 'Canned baked beans', 'cans', 1.20, 40, 5),
-(1, 'BBQ Sauce', 'Sweet Baby Ray BBQ sauce', 'cups', 0.75, 25, 5),
-(1, 'Brown Sugar', 'Brown sugar', 'oz', 0.05, 100, 10),
-(1, 'Jimmy Dean Sausage', 'Breakfast sausage', 'lb', 3.50, 25, 5),
+(1, 'Baked Beans', 'Canned baked beans', 9, 1.20, 40, 5), -- 'Per item'
+(1, 'BBQ Sauce', 'Sweet Baby Ray BBQ sauce', 5, 0.75, 25, 5), -- 'Cup'
+(1, 'Brown Sugar', 'Brown sugar', 2, 0.05, 100, 10), -- 'Ounce'
+(1, 'Jimmy Dean Sausage', 'Breakfast sausage', 1, 3.50, 25, 5), -- 'Pound'
 
 -- Green Chili Mac & Cheese Ingredients
-(1, 'Butter', 'Unsalted butter', 'sticks', 1.50, 50, 5),
-(1, 'Flour', 'All-purpose flour', 'cups', 0.10, 100, 10),
-(1, 'Milk', 'Whole milk', 'gallons', 3.00, 30, 5),
-(1, 'Salt', 'Salt', 'cups', 0.50, 100, 10),
-(1, 'Black Pepper', 'Ground black pepper', 'cups', 1.00, 50, 5),
-(1, 'Garlic Powder', 'Garlic powder', 'cups', 0.50, 40, 5),
-(1, 'Green Chili', 'Canned green chili', 'cans', 1.50, 30, 5),
-(1, 'Cheddar Cheese', 'Shredded cheddar cheese', 'bags', 5.00, 20, 5),
-(1, 'Parmesan Cheese', 'Shredded Parmesan cheese', 'cups', 3.50, 25, 5),
-(1, 'Blue Cheese', 'Crumbled blue cheese', 'cups', 4.50, 20, 5),
-(1, 'Macaroni Noodles', 'Macaroni pasta', 'oz', 0.75, 40, 10),
+(1, 'Butter', 'Unsalted butter', 9, 1.50, 50, 5), -- 'Per item'
+(1, 'Flour', 'All-purpose flour', 5, 0.10, 100, 10), -- 'Cup'
+(1, 'Milk', 'Whole milk', 8, 3.00, 30, 5), -- 'Gallon'
+(1, 'Salt', 'Salt', 5, 0.50, 100, 10), -- 'Cup'
+(1, 'Black Pepper', 'Ground black pepper', 5, 1.00, 50, 5), -- 'Cup'
+(1, 'Garlic Powder', 'Garlic powder', 5, 0.50, 40, 5), -- 'Cup'
+(1, 'Green Chili', 'Canned green chili', 9, 1.50, 30, 5), -- 'Per item'
+(1, 'Cheddar Cheese', 'Shredded cheddar cheese', 9, 5.00, 20, 5), -- 'Per item'
+(1, 'Parmesan Cheese', 'Shredded Parmesan cheese', 5, 3.50, 25, 5), -- 'Cup'
+(1, 'Blue Cheese', 'Crumbled blue cheese', 5, 4.50, 20, 5), -- 'Cup'
+(1, 'Macaroni Noodles', 'Macaroni pasta', 2, 0.75, 40, 10), -- 'Ounce'
 
 -- Ribs Ingredients
-(1, 'Pork Ribs', 'Fresh pork ribs', 'slabs', 10.00, 15, 5),
-(1, 'Apple Cider Vinegar', 'Apple cider vinegar', 'oz', 0.05, 100, 10),
-(1, 'Paprika', 'Ground paprika', 'cups', 1.00, 40, 5),
-(1, 'Cayenne Pepper', 'Ground cayenne pepper', 'cups', 1.50, 30, 5),
--- (1, 'Brown Sugar', 'Brown sugar', 'cups', 0.05, 100, 10),
+(1, 'Pork Ribs', 'Fresh pork ribs', 9, 10.00, 15, 5), -- 'Per item'
+(1, 'Apple Cider Vinegar', 'Apple cider vinegar', 2, 0.05, 100, 10), -- 'Ounce'
+(1, 'Paprika', 'Ground paprika', 5, 1.00, 40, 5), -- 'Cup'
+(1, 'Cayenne Pepper', 'Ground cayenne pepper', 5, 1.50, 30, 5), -- 'Cup'
 
 -- Wings Ingredients
-(1, 'Chicken Wings', 'Fresh chicken wings', 'lbs', 2.00, 30, 10),
+(1, 'Chicken Wings', 'Fresh chicken wings', 1, 2.00, 30, 10), -- 'Pound'
 
 -- Hotlinks Ingredients
-(1, 'Hotlinks', 'Spicy hotlink sausage', 'link', 4.50, 20, 5),
+(1, 'Hotlinks', 'Spicy hotlink sausage', 9, 4.50, 20, 5), -- 'Per item'
 
 -- Catfish Ingredients
-(1, 'Catfish Fillet', 'Fresh catfish fillet', 'pieces', 5.00, 10, 5),
+(1, 'Catfish Fillet', 'Fresh catfish fillet', 9, 5.00, 10, 5), -- 'Per item'
 
 -- Seasonings
-(1, 'Seasoning Mix', 'House seasoning mix', 'cups', 2.00, 20, 5);
+(1, 'Seasoning Mix', 'House seasoning mix', 5, 2.00, 20, 5); -- 'Cup'
 
 
 
@@ -164,48 +177,66 @@ VALUES
 
 -- @block
 -- Populate MenuItemComponents table (sample relationships)
-INSERT INTO MenuItemComponents (menu_item_id, inventory_item_id, quantity_required, unit)
+INSERT INTO MenuItemComponents (menu_item_id, inventory_item_id, quantity_required)
 VALUES
--- Great Granny's Green Beans (Menu Item ID: 3)
-(3, 1, 2.0, 'cans'), -- Canned Green Beans
-(3, 2, 1.25, 'cups'), -- Bullion
-(3, 3, 3.0, 'pieces'), -- Potatoes
-(3, 4, 3.0, 'strips'), -- Bacon
-
 -- Sweet Baby Baked Beans (Menu Item ID: 1)
-(1, 5, 2.0, 'cans'), -- Baked Beans
-(1, 6, 1.25, 'cups'), -- BBQ Sauce
-(1, 7, 1.5, 'oz'), -- Brown Sugar
-(1, 8, 1.0, 'lb'), -- Jimmy Dean Sausage
+(1, 5, 0.5), -- Baked Beans
+(1, 6, 0.1), -- BBQ Sauce
+(1, 7, 0.1), -- Brown Sugar
+(1, 8, 0.1), -- Jimmy Dean Sausage
+
+-- Teriyaki Parmesean Corn (Menu Item ID: 2)
+-- None for now
+
+-- Great Granny's Green Beans (Menu Item ID: 3)
+(3, 1, 0.5), -- Canned Green Beans
+(3, 2, 0.25), -- Bullion
+(3, 3, 3.0), -- Potatoes
+(3, 4, 3.0), -- Bacon
 
 -- Green Chili Mac & Cheese (Menu Item ID: 4)
-(4, 9, 2.5, 'sticks'), -- Butter
-(4, 10, 2.0, 'cups'), -- Flour
-(4, 11, 4.0, 'gallons'), -- Milk
-(4, 12, 1.25, 'cups'), -- Salt
-(4, 13, 0.5, 'cups'), -- Black Pepper
-(4, 14, 0.75, 'cups'), -- Garlic Powder
-(4, 15, 1.0, 'can'), -- Green Chili
-(4, 16, 1.0, 'bag'), -- Cheddar Cheese
-(4, 17, 5.0, 'cups'), -- Parmesan Cheese
-(4, 18, 4.0, 'cups'), -- Blue Cheese
-(4, 19, 8.0, 'oz'), -- Macaroni Noodles
+(4, 9, 0.25), -- Butter
+(4, 10, 0.25), -- Flour
+(4, 11, 0.25), -- Milk
+(4, 12, 0.25), -- Salt
+(4, 13, 0.25), -- Black Pepper
+(4, 14, 0.25), -- Garlic Powder
+(4, 15, 0.25), -- Green Chili
+(4, 16, 0.25), -- Cheddar Cheese
+(4, 17, 0.25), -- Parmesan Cheese
+(4, 18, 0.25), -- Blue Cheese
+(4, 19, 0.25), -- Macaroni Noodles
 
--- Rib Plate (Menu Item ID: 14)
-(14, 20, 4.0, 'slabs'), -- Pork Ribs
-(14, 21, 1.0, 'oz'), -- Apple Cider Vinegar
-(14, 22, 8.0, 'cups'), -- Paprika
-(14, 23, 1.5, 'cups'), -- Cayenne Pepper
-(14, 24, 4.0, 'cups'), -- Brown Sugar
+-- Wing Wednesday (Menu Item ID: 5)
+
+-- Wings a la Carte (Menu Item ID: 6)
+
+-- extra sauce (Menu Item ID: 7)
+
+-- Ribs a la Carte (Menu Item ID: 8)
+
+-- Hotlink a la Carte (Menu Item ID: 9)
+
+-- Fountain Drinks (Menu Item ID: 10)
+
+-- Cooler Drinks (Menu Item ID: 11)
+
+-- Little Man Ice Cream (Menu Item ID: 12)
 
 -- Wing Plate (Menu Item ID: 13)
-(13, 25, 6.0, 'lbs'), -- Chicken Wings
+(13, 25, 6.0), -- Chicken Wings
+
+-- Rib Plate (Menu Item ID: 14)
+(14, 20, 4.0), -- Pork Ribs
+(14, 21, 0.25), -- Apple Cider Vinegar
+(14, 22, 0.25), -- Paprika
+(14, 23, 0.25), -- Cayenne Pepper
 
 -- Hotlink Plate (Menu Item ID: 15)
-(15, 26, 1.0, 'link'), -- Hotlink
+(15, 24, 1.0), -- Hotlink
 
 -- Catfish Plate (Menu Item ID: 16)
-(16, 27, 1.0, 'pieces'); -- Catfish Fillet
+(16, 25, 1.0); -- Catfish Fillet
 
 -- @block
 SELECT * FROM MenuItems;
@@ -216,6 +247,8 @@ SELECT * FROM InventoryItems;
 -- @block
 SELECT * FROM MenuItemComponents;
 
+
+-- DROP TABLES BLOCKS
 -- @block 
 DROP TABLE MenuItemComponents;
 
