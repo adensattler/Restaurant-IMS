8f440686003f0c4f23454f2a61462b7722819970
Aden Sattler <adensattler@gmail.com>
2024-09-29 18:28:37 -0600

    default to run on 8080

--- wsgi.py
+++ wsgi.py
@@ -7,5 +7,4 @@ load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(getenv("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(getenv("PORT", 8080)))
